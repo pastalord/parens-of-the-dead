cccaa63a580ae94e515e3fecae228333fe34a63b
Seinfield Method <dont-break-the-chain@Seinfields-MacBook-Pro.local>
2016-01-23 14:55:18 -0600

    stuff

---
+++ src/undead/system.clj
@@ -0,0 +1,10 @@
+(ns undead.system
+ (:require [org.http-kit.server :refer [run-server]]))
+
+ (defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+(defn -main [& args]
+  (run-server app {:port 8080})
+  (println "Started server on localhost:8080"))
